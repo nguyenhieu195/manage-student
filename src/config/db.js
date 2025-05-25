c089acfb0fb2e446e35133470ce158a6cf9b3321
nguyenhieu195 <nguyenhieu1952005@gmail.com>
2025-05-25 20:44:52 +0700

    up

--- src/config/db.js
+++ src/config/db.js
@@ -2,7 +2,6 @@ import mysql from "mysql2/promise"
 import dotenv from "dotenv"
 
 dotenv.config()
-
 // Tạo pool connection
 const pool = mysql.createPool({
   host: process.env.DB_HOST || "localhost",
